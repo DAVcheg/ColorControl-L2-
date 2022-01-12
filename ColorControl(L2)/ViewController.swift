7c348c8dc8f5eb078d31bc24be8aa6ad09ea0d46
Aleksey Dergunov <davcheg@me.com>
2022-01-12 16:18:44 +1000

    Initial Commit

---
+++ ColorControl(L2)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorControl(L2)
+//
+//  Created by DAV on 12.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
