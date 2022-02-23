ea5cf1bc3c631a40ef10b58d4e0a183637c6d347
Aleksey Dergunov <davcheg@me.com>
2022-02-23 22:21:34 +1000

    Change settingsViewController, Fix constrain and code optimization

--- ColorControl(L2)/BackgroundSettingsViewController.swift
+++ ColorControl(L2)/BackgroundSettingsViewController.swift
@@ -7,6 +7,9 @@
 
 import UIKit
 
+//Protocol BackgroundLink {
+//
+//}
 
 class BackgroundSettingsViewController: UIViewController {
     
@@ -15,6 +18,10 @@ class BackgroundSettingsViewController: UIViewController {
     @IBOutlet var redValueLabel: UILabel!
     @IBOutlet var greenValueLabel: UILabel!
     @IBOutlet var blueValueLabel: UILabel!
+    
+    @IBOutlet var redValueTextEdit: UITextField!
+    @IBOutlet var greenValueTextEdit: UITextField!
+    @IBOutlet var blueValueTextEdit: UITextField!
     
     @IBOutlet var colorView: UIView!
     
@@ -35,25 +42,28 @@ class BackgroundSettingsViewController: UIViewController {
     }
     
     override func viewDidLayoutSubviews() {
-        colorView.layer.cornerRadius = colorView.frame.width / 2
+        colorView.layer.cornerRadius = colorView.frame.width / 10
     }
     
     // MARK: - IB Actions
     
-    @IBAction func redSliderAction() {
+    @IBAction func SliderAction() {
         redValueLabel.text = String(round(redSlider.value * 100)/100)
-        colorViewAction()
-    }
-    
-    @IBAction func greenSliderAction() {
+        redValueTextEdit.text = redValueLabel.text
         greenValueLabel.text = String(round(greenSlider.value * 100)/100)
+        greenValueTextEdit.text = greenValueLabel.text
+        blueValueLabel.text = String(round(blueSlider.value * 100)/100)
+        blueValueTextEdit.text = blueValueLabel.text
         colorViewAction()
     }
     
-    @IBAction func blueSliderAction() {
-        blueValueLabel.text = String(round(blueSlider.value * 100)/100)
-        colorViewAction()
+    @IBAction func TextEditAction(_ sender: Any) {
+        redSlider.value = Float(redValueTextEdit.text!) ?? 0.00
+        greenSlider.value = Float(greenValueTextEdit.text!) ?? 0.00
+        blueSlider.value = Float(blueValueTextEdit.text!) ?? 0.00
+        SliderAction()
     }
+    
     
 }
     
@@ -61,19 +71,14 @@ class BackgroundSettingsViewController: UIViewController {
 
 extension BackgroundSettingsViewController {
 private func assignbackground(){
-        //создаем константу с указанием изображения в assets
         let background = UIImage(named: "shtrihi-cveta-color")
-        // создаем экземпляр класса работы с изображениями и передаем нужный view для работы
         let imageView = UIImageView(frame: view.bounds)
-        // выбираем тип масштабирования изображения
         imageView.contentMode =  UIView.ContentMode.scaleAspectFill
         //разрешаем обрезку изображения по границам view
         imageView.clipsToBounds = true
-        //указываем какой использовать фон
         imageView.image = background
-        //устанавливаем изображение по центру
         imageView.center = view.center
-        //передаем параметры в метод addSubview (создание нового слоя)
+        //передаем аргумент в метод addSubview (создание нового слоя)
         view.addSubview(imageView)
         //метод переноса нового слоя на задний фон
         self.view.sendSubviewToBack(imageView)
